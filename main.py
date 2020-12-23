05f7a2585a881e43bc0b911fe129f3a258f59682
Alejandro Largo <largoalejo@gmail.com>
2020-12-23 12:14:46 -0500

    V1.1 ordenador de archivos, si el archivo ya existe en la carpeta lo remplaza con el archivo nuevo

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
-import os
-import sys
+import os.path
 import shutil
-import time
-
+from pathlib import Path
 
 downloadPath = os.path.abspath('/home/alejandro/Descargas/')
 downloadPath += '/'
-videoExt = ['.mov', '.mp4']
+mediaExt = ['.mov', '.mp4', '.ogg', '.mp3']
 imgExt = ['.png', '.jpg', '.jpeg', '.gif']
 textExt = ['.txt', '.docx', '.doc', '.pdf']
 packExt = ['.deb', '.AppImage']
 
 def sort(file, ext):
-    for i in videoExt:
-        if ext == i:
-            shutil.move(downloadPath + file, downloadPath + 'videos')
+    filePath = Path(downloadPath + file)
+    if filePath.is_file() or filePath.is_dir():
+        for i in mediaExt:
+            if ext == i:
+                shutil.copy(downloadPath + file, downloadPath + 'multi-media')
+                os.remove(downloadPath + file)
 
-    for i in imgExt:
-        if ext == i:
-            shutil.move(downloadPath + file, downloadPath + 'imagenes')
+        for i in imgExt:
+            if ext == i:
+                shutil.copy(downloadPath + file, downloadPath + 'imagenes')
+                os.remove(downloadPath + file)
 
-    for i in textExt:
-        if ext == i:
-            shutil.move(downloadPath + file, downloadPath + 'texto')
+        for i in textExt:
+            if ext == i:
+                shutil.copy(downloadPath + file, downloadPath + 'texto')
+                os.remove(downloadPath + file)
 
-    for i in packExt:
-        if ext == i:
-            shutil.move(downloadPath + file, downloadPath + 'paquetes')
+        for i in packExt:
+            if ext == i:
+                shutil.copy(downloadPath + file, downloadPath + 'paquetes')
+                os.remove(downloadPath + file)
 
-    if ext != '':
-        shutil.move(downloadPath + file, downloadPath + 'otros')
-    
-    if ext == '' and file != 'videos' and file != 'imagenes' and file != 'texto' and file != 'paquetes':
-        shutil.move(downloadPath + file, downloadPath + 'otros')
+    if filePath.is_file():
+        if ext != '':
+            shutil.copy(downloadPath + file, downloadPath + 'otros')
+            os.remove(downloadPath + file)
+        
+        if ext == '' and file != 'videos' and file != 'imagenes' and file != 'texto' and file != 'paquetes':
+            shutil.copy(downloadPath + file, downloadPath + 'otros')
+            os.remove(downloadPath + file)
 
 if __name__ == "__main__":
     for file in os.listdir(downloadPath):
