bf51d223b38accef61c0959e542003cf672a02ab
Alejandro Largo <largoalejo@gmail.com>
2020-12-26 12:28:45 -0500

    V2.0 interfaz grafica

--- index.py
+++ index.py
@@ -3,86 +3,87 @@
 import os.path
 import shutil
 from pathlib import Path
+from tkinter import ttk
+from tkinter import filedialog
+from tkinter import *
 
-downloadPath = os.path.abspath('/home/alejandro/Descargas/')
-downloadPath += '/'
+class App:
+    def __init__(self, window):
+        self.window = window
+        self.window.title('Sort Directories')
+        self.sort_path = ''
 
-multi = 'multi-media'
-img = 'imagenes'
-txt = 'texto'
-packages = 'paquetes'
-code = 'codigo'
-others = 'otros'
+        self.dirs = ['media', 'img', 'text', 'packages', 'code', 'other']
+        self.exts = [
+            ['.mov', '.mp4', '.ogg', '.mp3'],
+            ['.png', '.jpg', '.jpeg', '.gif'],
+            ['.txt', '.docx', '.doc', '.pdf'],
+            ['.deb', '.AppImage'],
+            ['.py', '.c', '.cpp', '.js', '.css', '.html', '.json', 'cs']
+        ]
 
+        # Browser
+        self.browse_label = Label(master = window, text = 'Browse your directory:')
+        self.browse_label.grid(row = 0, column = 0)
+        self.folder_path = StringVar(value = 'Folder path...')
+        self.folder_label = Label(master = window, borderwidth = 2, relief = 'sunken', textvariable = self.folder_path)
+        self.folder_label.config(bg = 'white')
+        self.folder_label.grid(row = 1, column = 0)
+        self.browse_button = Button(text = 'Explore', command = self.browse_button)
+        self.browse_button.grid(row = 1, column = 1)
 
-# Make dirs
-if not os.path.exists(downloadPath + multi):
-    os.mkdir(downloadPath + multi)
+        # Make directories
+        self.mkdir_label = Label(master = window, text = 'Make directories:')
+        self.mkdir_label.grid(row = 2, column = 0)
+        self.mkdir_button = Button(text = 'Make directories', command = self.make_dirs)
+        self.mkdir_button.grid(row = 3, columnspan = 2, sticky = W + E)
 
-if not os.path.exists(downloadPath + img):
-    os.mkdir(downloadPath + img)
-
-if not os.path.exists(downloadPath + txt):
-    os.mkdir(downloadPath + txt)
-
-if not os.path.exists(downloadPath + packages):
-    os.mkdir(downloadPath + packages)
-
-if not os.path.exists(downloadPath + others):
-    os.mkdir(downloadPath + others)
-
-if not os.path.exists(downloadPath + code):
-    os.mkdir(downloadPath + code)
-
-
-# Extensions
-mediaExt = ['.mov', '.mp4', '.ogg', '.mp3']
-imgExt = ['.png', '.jpg', '.jpeg', '.gif']
-textExt = ['.txt', '.docx', '.doc', '.pdf']
-packExt = ['.deb', '.AppImage']
-codExt = ['.py', '.c', '.cpp', '.js', '.css', '.html', '.json', '.cs']
-
-
-def sort(file, ext):
-    filePath = Path(downloadPath + file)
-    if filePath.is_file() or filePath.is_dir():
-        for i in mediaExt:
-            if ext == i:
-                shutil.copy(downloadPath + file, downloadPath + multi)
-                os.remove(downloadPath + file)
-
-        for i in imgExt:
-            if ext == i:
-                shutil.copy(downloadPath + file, downloadPath + img)
-                os.remove(downloadPath + file)
-
-        for i in textExt:
-            if ext == i:
-                shutil.copy(downloadPath + file, downloadPath + txt)
-                os.remove(downloadPath + file)
-
-        for i in packExt:
-            if ext == i:
-                shutil.copy(downloadPath + file, downloadPath + packages)
-                os.remove(downloadPath + file)
-
-        for i in codExt:
-            if ext == i:
-                shutil.copy(downloadPath + file, downloadPath + code)
-                os.remove(downloadPath + file)
-
-    # other extension
-    if filePath.is_file():
-        if ext != '':
-            shutil.copy(downloadPath + file, downloadPath + others)
-            os.remove(downloadPath + file)
+        # Sort files
+        self.sort_button = Button(text = 'Sort files', command = self.sort)
+        self.sort_button.grid(row = 4, columnspan = 2, sticky = W + E)
         
-        if ext == '' and file != multi and file != img and file != txt and file != packages and file != code:
-            shutil.copy(downloadPath + file, downloadPath + others)
-            os.remove(downloadPath + file)
+
+    def browse_button(self):
+        dirname = filedialog.askdirectory()
+        self.folder_path.set(dirname)
+        self.sort_path = os.path.abspath(str(self.folder_path.get()))
+        self.sort_path += '/'
+        # print(self.sort_path)
+
+    def make_dirs(self):
+        for dir in self.dirs:
+            if not os.path.exists(self.sort_path + dir):
+                os.mkdir(self.sort_path + dir)
 
 
-if __name__ == "__main__":
-    for file in os.listdir(downloadPath):
-        fileName, ext = os.path.splitext(file)
-        sort(file, ext)
+    def sort_files(self, file, ext):
+        file_path = Path(self.sort_path + file)
+        if file_path.is_file() or file_path.is_dir():
+            for ls in self.exts:
+                for ex in ls:
+                    if ex == ext:
+                        shutil.copy(self.sort_path + file, self.sort_path + self.dirs[self.exts.index(ls)])
+                        os.remove(self.sort_path + file)
+
+        # other extension
+        if file_path.is_file():
+            if ext != '':
+                shutil.copy(self.sort_path + file, self.sort_path + self.dirs[5])
+                os.remove(self.sort_path + file)
+            
+            if ext == '' and file != self.dirs[0] and file != self.dirs[1] and file != self.dirs[2] and file != self.dirs[3] and file != self.dirs[4]:
+                shutil.copy(self.sort_path + file, self.sort_path + self.dirs[5])
+                os.remove(self.sort_path + file)
+
+
+    def sort(self):
+        for file in os.listdir(self.sort_path):
+            fileName, ext = os.path.splitext(file)
+            self.sort_files(file, ext)
+        
+
+
+if __name__ == '__main__':
+    window = Tk()
+    application = App(window)
+    window.mainloop()
